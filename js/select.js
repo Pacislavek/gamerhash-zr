e74051dae9c9936f338e59bb315b6357750d680c
Patryk Kowalski <pacislavek@gmail.com>
2021-06-03 17:48:36 +0200

    general update style for select

--- js/select.js
+++ js/select.js
@@ -4,7 +4,7 @@ function formatState(state) {
   }
   var baseUrl = "img/cryptocurrency";
   var $state = $(
-    '<div style="display:flex; flex-direction: row; align-items: center; justify-content: space-between"><span><img src="' + baseUrl + '/crypto-' + state.element.value.toLowerCase() + '.svg" class="img-flag" /> ' + state.text + '</span><div><span style="margin-right: 20px;">' + state.bitcoinValue + 'BTC' + '</span><span>' + state.usdValue + 'USD' + '</span></div></div>'
+    '<div style="display:flex; flex-direction: row; align-items: center; justify-content: space-between"><span style="font-weight: 600; color: #181B1F;"><img style="margin-right: 15px;" src="' + baseUrl + '/crypto-' + state.element.value.toLowerCase() + '.svg" class="img-flag" /> ' + state.text + '</span><div><span style="margin-right: 10px; color: #181B1F;">' + state.bitcoinValue + 'BTC' + '</span><span style="color=#737373;">' + state.usdValue + 'USD' + '</span></div></div>'
   );
 
   return $state;
